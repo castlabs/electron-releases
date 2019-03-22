7cfbeced78f7ff2bab6d53e76856b7ecdecf48b9
Emil Pettersson <khwaaj@gmail.com>
2019-03-22 12:45:29 +0100

    Updated README.md, electron.d.ts & package.json for v3.1.7-wvvmp

--- electron.d.ts
+++ electron.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Electron 3.1.6-wvvmp
+// Type definitions for Electron 3.1.7-wvvmp
 // Project: http://electronjs.org/
 // Definitions by: The Electron Team <https://github.com/electron/electron>
 // Definitions: https://github.com/electron/electron-typescript-definitions
