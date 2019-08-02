c16644ebd7511654306c96de2bf72e320d39fcf5
Emil Pettersson <khwaaj@gmail.com>
2019-08-02 09:09:44 +0200

    Updated for v3.1.13-wvvmp

--- install.js
+++ install.js
@@ -1,29 +1,26 @@
 #!/usr/bin/env node
 
-// maintainer note - x.y.z-ab version in package.json -> x.y.z
-var version = require("./package").version;
+var version = require('./package').version
 
-var fs = require("fs");
-var os = require("os");
-var path = require("path");
-var extract = require("extract-zip");
-var download = require("electron-download");
+var fs = require('fs')
+var os = require('os')
+var path = require('path')
+var extract = require('extract-zip')
+var download = require('electron-download')
 
-var installedVersion = null;
+var installedVersion = null
 try {
-  installedVersion = fs.readFileSync(path.join(__dirname, "dist", "version"), "utf-8").replace(/^v/, "");
+  installedVersion = fs.readFileSync(path.join(__dirname, 'dist', 'version'), 'utf-8').replace(/^v/, '')
 } catch (ignored) {
   // do nothing
 }
 
-if (process.env.DONT_INSTALL_ELECTRON == "1") {
-  process.exit(0);
-}
+var platformPath = getPlatformPath()
 
-var platformPath = getPlatformPath();
+var electronPath = process.env.ELECTRON_OVERRIDE_DIST_PATH || path.join(__dirname, 'dist', platformPath)
 
-if (installedVersion === version && fs.existsSync(path.join(__dirname, platformPath))) {
-  process.exit(0);
+if (installedVersion === version && fs.existsSync(electronPath)) {
+  process.exit(0)
 }
 
 // downloads if not cached
@@ -33,43 +30,37 @@ download({
   version: version,
   platform: process.env.npm_config_platform,
   arch: process.env.npm_config_arch,
-  strictSSL: process.env.npm_config_strict_ssl === "true",
-  force: process.env.force_no_cache === "true",
-  quiet: ["info", "verbose", "silly", "http"].indexOf(process.env.npm_config_loglevel) === -1
-}, extractFile);
+  strictSSL: process.env.npm_config_strict_ssl === 'true',
+  force: process.env.force_no_cache === 'true',
+  quiet: process.env.npm_config_loglevel === 'silent' || process.env.CI
+}, extractFile)
 
 // unzips and makes path.txt point at the correct executable
 function extractFile (err, zipPath) {
-  if (err) {
-    return onerror(err);
-  }
-  extract(zipPath, {dir: path.join(__dirname, "dist")}, function (err) {
-    if (err) {
-      return onerror(err);
-    }
-    fs.writeFile(path.join(__dirname, "path.txt"), platformPath, function (err) {
-      if (err) {
-        return onerror(err);
-      }
-    });
-  });
+  if (err) return onerror(err)
+  extract(zipPath, {dir: path.join(__dirname, 'dist')}, function (err) {
+    if (err) return onerror(err)
+    fs.writeFile(path.join(__dirname, 'path.txt'), platformPath, function (err) {
+      if (err) return onerror(err)
+    })
+  })
 }
 
 function onerror (err) {
-  throw err;
+  throw err
 }
 
 function getPlatformPath () {
-  var platform = process.env.npm_config_platform || os.platform();
+  var platform = process.env.npm_config_platform || os.platform()
 
   switch (platform) {
-    case "darwin":
-      return "dist/Electron.app/Contents/MacOS/Electron";
-    case "freebsd":
-    case "linux":
-      return "dist/electron";
-    case "win32":
-      return "dist/electron.exe";
+    case 'darwin':
+      return 'Electron.app/Contents/MacOS/Electron'
+    case 'freebsd':
+    case 'linux':
+      return 'electron'
+    case 'win32':
+      return 'electron.exe'
     default:
       throw new Error('Electron builds are not available on platform: ' + platform)
   }
