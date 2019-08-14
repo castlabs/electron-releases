99dbcbf257ae8ad27408d599043737c1b2522baa
Emil Pettersson <khwaaj@gmail.com>
2019-08-14 09:12:06 +0200

    Updated for v6.0.2-wvvmp

--- electron.d.ts
+++ electron.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Electron 6.0.1
+// Type definitions for Electron 6.0.2
 // Project: http://electronjs.org/
 // Definitions by: The Electron Team <https://github.com/electron/electron>
 // Definitions: https://github.com/electron/electron-typescript-definitions
@@ -1851,9 +1851,6 @@ declare namespace Electron {
      * window.
      */
     getTitle(): string;
-    /**
-     * On Windows and Linux always returns true.
-     */
     hasShadow(): boolean;
     /**
      * Hides the window.
@@ -2055,7 +2052,7 @@ declare namespace Electron {
      */
     setFullScreenable(fullscreenable: boolean): void;
     /**
-     * Sets whether the window should have a shadow. On Windows and Linux does nothing.
+     * Sets whether the window should have a shadow.
      */
     setHasShadow(hasShadow: boolean): void;
     /**
@@ -10313,6 +10310,7 @@ declare namespace Electron {
 
   interface VerifyWidevineCdmOptions {
     session?: Session;
+    disableUpdate?: boolean;
   }
 
   interface Versions {
