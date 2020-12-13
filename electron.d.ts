6051585cf965a8f21745dc9bd14542976995082d
Emil Pettersson <khwaaj@gmail.com>
2020-12-13 12:39:04 +0100

    Updated for v11.1.0-wvvmp

--- electron.d.ts
+++ electron.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Electron 11.0.5
+// Type definitions for Electron 11.1.0
 // Project: http://electronjs.org/
 // Definitions by: The Electron Team <https://github.com/electron/electron>
 // Definitions: https://github.com/electron/electron-typescript-definitions
@@ -4978,6 +4978,10 @@ Retrieves the product descriptions.
      */
     ports: MessagePortMain[];
     /**
+     * The internal ID of the renderer process that sent this message
+     */
+    processId: number;
+    /**
      * A function that will send an IPC message to the renderer frame that sent the
      * original message that you are currently handling.  You should use this method to
      * "reply" to the sent message in order to guarantee the reply will go to the
@@ -5002,6 +5006,10 @@ Retrieves the product descriptions.
      * The ID of the renderer frame that sent this message
      */
     frameId: number;
+    /**
+     * The internal ID of the renderer process that sent this message
+     */
+    processId: number;
     /**
      * Returns the `webContents` that sent the message
      */
@@ -10278,7 +10286,7 @@ An example of sending messages from the main process to the renderer process:
      * 
 You can also read `frameId` from all incoming IPC messages in the main process.
      */
-    sendToFrame(frameId: number, channel: string, ...args: any[]): void;
+    sendToFrame(frameId: (number) | ([number, number]), channel: string, ...args: any[]): void;
     /**
      * Mute the audio on the current web page.
      */
