5e2d59dd79ac577e3607d99644fa813823c41488
Emil Pettersson <khwaaj@gmail.com>
2021-05-11 13:47:13 +0200

    Updated for v13.0.0-wvvmp-beta.24

--- electron.d.ts
+++ electron.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Electron 13.0.0-beta.23
+// Type definitions for Electron 13.0.0-beta.24
 // Project: http://electronjs.org/
 // Definitions by: The Electron Team <https://github.com/electron/electron>
 // Definitions: https://github.com/electron/electron-typescript-definitions
