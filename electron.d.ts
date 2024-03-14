4445e4eee744033c14d071ee6274006910f67bad
Emil Pettersson <khwaaj@gmail.com>
2024-03-14 01:14:24 +0100

    Updated for v29.1.2+wvcus

--- electron.d.ts
+++ electron.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Electron 29.1.1+wcus
+// Type definitions for Electron 29.1.2+wcus
 // Project: http://electronjs.org/
 // Definitions by: The Electron Team <https://github.com/electron/electron>
 // Definitions: https://github.com/electron/typescript-definitions
@@ -6885,12 +6885,14 @@ declare namespace Electron {
      */
     static createFromBuffer(buffer: Buffer, options?: CreateFromBufferOptions): NativeImage;
     /**
-     * Creates a new `NativeImage` instance from `dataURL`.
+     * Creates a new `NativeImage` instance from `dataUrl`, a base 64 encoded Data URL
+     * string.
      */
     static createFromDataURL(dataURL: string): NativeImage;
     /**
-     * Creates a new `NativeImage` instance from the NSImage that maps to the given
-     * image name. See `System Icons` for a list of possible values.
+     * Creates a new `NativeImage` instance from the `NSImage` that maps to the given
+     * image name. See Apple's `NSImageName` documentation for a list of possible
+     * values.
      *
      * The `hslShift` is applied to the image with the following rules:
      *
@@ -6911,9 +6913,6 @@ declare namespace Electron {
      * actually be `NSFolder`. Therefore, you'll need to determine the correct string
      * representation for your image before passing it in. This can be done with the
      * following:
-     *
-     * `echo -e '#import <Cocoa/Cocoa.h>\nint main() { NSLog(@"%@", SYSTEM_IMAGE_NAME);
-     * }' | clang -otest -x objective-c -framework Cocoa - && ./test`
      *
      * where `SYSTEM_IMAGE_NAME` should be replaced with any value from this list.
      *
@@ -6937,8 +6936,8 @@ declare namespace Electron {
     static createThumbnailFromPath(path: string, size: Size): Promise<Electron.NativeImage>;
     /**
      * Add an image representation for a specific scale factor. This can be used to
-     * explicitly add different scale factor representations to an image. This can be
-     * called on empty images.
+     * programmatically add different scale factor representations to an image. This
+     * can be called on empty images.
      */
     addRepresentation(options: AddRepresentationOptions): void;
     /**
@@ -6946,7 +6945,7 @@ declare namespace Electron {
      */
     crop(rect: Rectangle): NativeImage;
     /**
-     * The image's aspect ratio.
+     * The image's aspect ratio (width divided by height).
      *
      * If `scaleFactor` is passed, this will return the aspect ratio corresponding to
      * the image representation most closely matching the passed value.
@@ -6962,7 +6961,7 @@ declare namespace Electron {
     getBitmap(options?: BitmapOptions): Buffer;
     /**
      * A Buffer that stores C pointer to underlying native handle of the image. On
-     * macOS, a pointer to `NSImage` instance would be returned.
+     * macOS, a pointer to `NSImage` instance is returned.
      *
      * Notice that the returned pointer is a weak pointer to the underlying native
      * image instead of a copy, so you _must_ ensure that the associated `nativeImage`
@@ -6973,7 +6972,7 @@ declare namespace Electron {
     getNativeHandle(): Buffer;
     /**
      * An array of all scale factors corresponding to representations for a given
-     * nativeImage.
+     * `NativeImage`.
      */
     getScaleFactors(): number[];
     /**
@@ -6986,7 +6985,7 @@ declare namespace Electron {
      */
     isEmpty(): boolean;
     /**
-     * Whether the image is a template image.
+     * Whether the image is a macOS template image.
      */
     isTemplateImage(): boolean;
     /**
@@ -6997,7 +6996,7 @@ declare namespace Electron {
      */
     resize(options: ResizeOptions): NativeImage;
     /**
-     * Marks the image as a template image.
+     * Marks the image as a macOS template image.
      */
     setTemplateImage(option: boolean): void;
     /**
@@ -7005,7 +7004,7 @@ declare namespace Electron {
      */
     toBitmap(options?: ToBitmapOptions): Buffer;
     /**
-     * The data URL of the image.
+     * The Data URL of the image.
      */
     toDataURL(options?: ToDataURLOptions): string;
     /**
