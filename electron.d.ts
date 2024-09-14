ef5bdcc448fdf34423d4b011503be663f8a07457
Emil Pettersson <khwaaj@gmail.com>
2024-09-14 23:33:09 +0200

    Updated for v30.5.1+wvcus

--- electron.d.ts
+++ electron.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Electron 30.5.0+wcus
+// Type definitions for Electron 30.5.1+wcus
 // Project: http://electronjs.org/
 // Definitions by: The Electron Team <https://github.com/electron/electron>
 // Definitions: https://github.com/electron/typescript-definitions
