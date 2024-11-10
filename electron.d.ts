2b9740733df9d0c2a856d806b513712813bd7697
Emil Pettersson <khwaaj@gmail.com>
2024-11-10 03:15:05 +0100

    Updated for v34.0.0-alpha.8+wvcus

--- electron.d.ts
+++ electron.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Electron 34.0.0-alpha.7+wcus
+// Type definitions for Electron 34.0.0-alpha.8+wcus
 // Project: http://electronjs.org/
 // Definitions by: The Electron Team <https://github.com/electron/electron>
 // Definitions: https://github.com/electron/typescript-definitions
@@ -14112,32 +14112,32 @@ declare namespace Electron {
      */
     on(event: 'exit', listener: (
                                  /**
-                                  * Contains the exit code for the process obtained from waitpid on posix, or
-                                  * GetExitCodeProcess on windows.
+                                  * Contains the exit code for the process obtained from waitpid on POSIX, or
+                                  * GetExitCodeProcess on Windows.
                                   */
                                  code: number) => void): this;
     off(event: 'exit', listener: (
                                  /**
-                                  * Contains the exit code for the process obtained from waitpid on posix, or
-                                  * GetExitCodeProcess on windows.
+                                  * Contains the exit code for the process obtained from waitpid on POSIX, or
+                                  * GetExitCodeProcess on Windows.
                                   */
                                  code: number) => void): this;
     once(event: 'exit', listener: (
                                  /**
-                                  * Contains the exit code for the process obtained from waitpid on posix, or
-                                  * GetExitCodeProcess on windows.
+                                  * Contains the exit code for the process obtained from waitpid on POSIX, or
+                                  * GetExitCodeProcess on Windows.
                                   */
                                  code: number) => void): this;
     addListener(event: 'exit', listener: (
                                  /**
-                                  * Contains the exit code for the process obtained from waitpid on posix, or
-                                  * GetExitCodeProcess on windows.
+                                  * Contains the exit code for the process obtained from waitpid on POSIX, or
+                                  * GetExitCodeProcess on Windows.
                                   */
                                  code: number) => void): this;
     removeListener(event: 'exit', listener: (
                                  /**
-                                  * Contains the exit code for the process obtained from waitpid on posix, or
-                                  * GetExitCodeProcess on windows.
+                                  * Contains the exit code for the process obtained from waitpid on POSIX, or
+                                  * GetExitCodeProcess on Windows.
                                   */
                                  code: number) => void): this;
     /**
@@ -19212,7 +19212,7 @@ declare namespace Electron {
      */
     reason: ('clean-exit' | 'abnormal-exit' | 'killed' | 'crashed' | 'oom' | 'launch-failed' | 'integrity-failure');
     /**
-     * The exit code for the process (e.g. status from waitpid if on posix, from
+     * The exit code for the process (e.g. status from waitpid if on POSIX, from
      * GetExitCodeProcess on Windows).
      */
     exitCode: number;
@@ -19554,14 +19554,16 @@ declare namespace Electron {
 
   interface FromPartitionOptions {
     /**
-     * Whether to enable cache.
+     * Whether to enable cache. Default is `true` unless the `--disable-http-cache`
+     * switch is used.
      */
     cache: boolean;
   }
 
   interface FromPathOptions {
     /**
-     * Whether to enable cache.
+     * Whether to enable cache. Default is `true` unless the `--disable-http-cache`
+     * switch is used.
      */
     cache: boolean;
   }
