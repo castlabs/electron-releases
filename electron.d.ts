1b4c06c4d9ce81b612f6b10bc59150ccb52b6743
Emil Pettersson <khwaaj@gmail.com>
2026-07-02 18:02:00 +0200

    Updated for v41.9.2+wvcus

--- electron.d.ts
+++ electron.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Electron 41.7.1+wvcus
+// Type definitions for Electron 41.9.2+wvcus
 // Project: http://electronjs.org/
 // Definitions by: The Electron Team <https://github.com/electron/electron>
 // Definitions: https://github.com/electron/typescript-definitions
@@ -1669,6 +1669,22 @@ declare namespace Electron {
      * for `hostname`.
      */
     setClientCertRequestPasswordHandler(handler: (clientCertRequestParams: ClientCertRequestParams) => Promise<string>): void;
+    /**
+     * Sets the `.desktop` filename on Linux. This should match the base filename of
+     * the app's installed `.desktop` file. The `.desktop` suffix is optional.
+     *
+     * This value is used to determine the default XDG application ID on Wayland and
+     * `WM_CLASS` on X11. If it is not set, Electron will attempt to infer a name, but
+     * it may not match the packaged app's actual `.desktop` file. This could result in
+     * the app showing a generic icon or failing to respond to global keyboard
+     * shortcuts.
+     *
+     * This API must be called before the `ready` event. The value can also be set
+     * using `desktopName` in `package.json`.
+     *
+     * @platform linux
+     */
+    setDesktopName(name: string): void;
     /**
      * Sets or removes a custom Jump List for the application, and returns one of the
      * following strings:
@@ -7169,6 +7185,8 @@ declare namespace Electron {
      * support memory dumps yet)
      * * Click on one of the `M` symbols
      * * Click on a `☰` triple bar icon (e.g., in the `malloc` column)
+     *
+     * [Image: Screenshot showing how to view a heapdump in Chromium]
      *
      * @experimental
      */
@@ -17633,7 +17651,11 @@ declare namespace Electron {
      *
      * Before:
      *
+     * [Image: Image Before Text Selection Adjustment]
+     *
      * After:
+     *
+     * [Image: Image After Text Selection Adjustment]
      */
     adjustSelection(options: AdjustSelectionOptions): void;
     /**
